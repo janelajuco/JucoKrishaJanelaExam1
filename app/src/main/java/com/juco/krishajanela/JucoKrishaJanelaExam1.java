b0d2f3c628e9cd52d2fc732994c9e66bac3e5250
janelajuco <janelajuco@gmail.com>
2018-09-13 15:34:02 +0800

    Initial commit

---
+++ app/src/main/java/com/juco/krishajanela/JucoKrishaJanelaExam1.java
@@ -0,0 +1,13 @@
+package com.juco.krishajanela;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JucoKrishaJanelaExam1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_juco_krisha_janela_exam1);
+    }
+}
